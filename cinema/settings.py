7c29fe7d3c65ca4dc64899dc537809a14738d77a
apiut <apiut.toel@technomoto.biz>
2020-01-26 01:37:06 +0300

    chores: formated file according to pip guidelines

--- cinema/settings.py
+++ cinema/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+]   
 
 ROOT_URLCONF = 'cinema.urls'
 
